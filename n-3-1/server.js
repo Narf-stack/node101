58a6bbc50e3027dcb032d3a6d246928c192bc7be
Frantz Crozilhac <fran.stacksta@gmail.com>
2021-07-13 19:37:24 +0200

    Start 3-1

---
+++ n-3-1/server.js
@@ -0,0 +1,91 @@
+'use strict';
+const handlers = require('./handlers.js')
+
+// import the needed node_modules.
+const express = require('express');
+const morgan = require('morgan');
+
+express()
+  // Below are methods that are included in express(). We chain them for convenience.
+  // --------------------------------------------------------------------------------
+
+  // This will give us will log more info to the console. see https://www.npmjs.com/package/morgan
+  .use(morgan('tiny'))
+
+  // Any requests for static files will go into the public folder
+  .use(express.static('public'))
+
+  // Nothing to modify above this line
+  // ---------------------------------
+  // add new endpoints here 👇
+  .get('/cat-message', (req, res) => {
+    const message = { author: 'cat', text: 'Meow' };
+    const randomTime = Math.floor(Math.random() * 3000);
+    setTimeout(() => {
+      res.status(200).json({ status: 200, message });
+    }, randomTime);
+  })
+
+  .get('/monkey-message', (req, res) => {
+    const messages = [
+      'Don’t monkey around with me.',
+      'If you pay peanuts, you get monkeys.',
+      'I fling 💩 at you!',
+      '🙊',
+      '🙈',
+      '🙉',
+    ];
+
+    const randomTime = Math.floor(Math.random() * 3000);
+    const randomMessage = Math.floor(Math.random() * messages.length);
+    const message = { author: 'monkey', text: messages[randomMessage] };
+
+    setTimeout(() => {
+      res.status(200).json({ status: 200, message });
+    }, randomTime);
+
+  })
+
+  .get('/parrot-message', (req, res) => {
+    const message = { author: 'parrot', text: req.query.message };
+    const randomTime = Math.floor(Math.random() * 3000);
+
+    setTimeout(() => {
+      res.status(200).json({ status: 200, message });
+    }, randomTime);
+  })
+
+  .get('/bot-message', (req, res) => {
+    let botMessage = handlers.getBotMessage(req.query.message);
+
+    const message = { author: 'bot', text: botMessage };
+    const randomTime = Math.floor(Math.random() * 3000);
+
+    setTimeout(() => {
+      res.status(200).json({ status: 200, message });
+    }, randomTime);
+  })
+  // add new endpoints here ☝️
+  // ---------------------------------
+  // Nothing to modify below this line
+
+  // this serves up the homepage
+  .get('/', (req, res) => {
+    res
+      .status(200)
+      .json({ status: 200, message: "This is the homepage... it's empty :(" });
+  })
+
+  // this is our catch all endpoint. If a user navigates to any endpoint that is not
+  // defined above, they get to see our 404 page.
+  .get('*', (req, res) => {
+    res
+      .status(404)
+      .json({
+        status: 404,
+        message: 'This is obviously not the page you are looking for.',
+      });
+  })
+
+  // Node spins up our server and sets it to listen on port 8000.
+  .listen(8000, () => console.log(`Listening on port 8000`));
